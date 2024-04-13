de7ba95254ba3a51e1e01ea8f2bc30f0a27132b1
Alister <80782387+aalister@users.noreply.github.com>
2024-04-13 12:56:03 +0100

    Add file server

--- cmd/detective/main.go
+++ cmd/detective/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("frontend")))
+
+	if err := http.ListenAndServe(":5714", nil); err != nil {
+		log.Fatal(err)
+	}
 }
