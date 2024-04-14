4a58a04ca947634de47036bdca850425bd0fbd89
Alex <Alexg-153@users.noreply.github.com>
2024-04-14 02:12:11 +0100

    add personalities and prompts

---
+++ llm/prompts.go
@@ -0,0 +1,18 @@
+package llm
+
+var personalityPrompts = []string{
+	"What's your favorite song?",
+	"What type of music do you like to listen to?",
+	"What's your favorite movie?",
+	"What do you like to do in your spare time?",
+	"What makes you unique?",
+	"What did you want to be when you were a kid?",
+	"What was your favorite subject in school?",
+	"What would you do if you won the lottery?",
+	"What's a boring fact about you?",
+}
+
+var compsciPrompt = []string{
+	"What's your favorite programming language?",
+	"Is Java or C# better for OOP?",
+}
